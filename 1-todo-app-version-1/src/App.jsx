26ede4b98f5800fe6beac0363235ca4c5d0434e2
aayushmohan2099 <ayushmohan193@gmail.com>
2024-12-30 22:34:37 +0530

    Built-structure-of-todo-app

--- 1-todo-app-version-1/src/App.jsx
+++ 1-todo-app-version-1/src/App.jsx
@@ -1,5 +1,52 @@
 function App() {
-  return <center class='todo-container'>placeholder</center>
+  return (
+    <center class="todo-container">
+      <h1>TODO App by Ayush</h1>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">
+            <input type="text" placeholder="Enter Tasks here!" />
+          </div>
+          <div class="col-4">
+            <input type="date" />
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-success">
+              Add
+            </button>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-6">
+            Buy milk
+          </div>
+          <div class="col-4">
+            4/10/24
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-6">
+            Learn React
+          </div>
+          <div class="col-4">
+            30/12/24
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </center>
+  );
 }
 
-export default App
+export default App;
