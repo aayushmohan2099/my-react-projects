98c317ca2dd934add293e767d656a785f036555c
aayushmohan2099 <ayushmohan193@gmail.com>
2024-12-30 22:48:46 +0530

    Added-TodoItem-components

--- 1-todo-app-version-1/src/App.jsx
+++ 1-todo-app-version-1/src/App.jsx
@@ -1,39 +1,15 @@
 import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
+import TodoItem1 from "./components/TodoItem1";
+import TodoItem2 from "./components/TodoItem2";
 
 function App() {
   return (
     <center class="todo-container">
       <AppName />
       <AddTodo />
-
-        <div class="row">
-          <div class="col-6">
-            Buy milk
-          </div>
-          <div class="col-4">
-            4/10/24
-          </div>
-          <div class="col-2">
-            <button type="button" class="btn btn-danger">
-              Delete
-            </button>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-6">
-            Learn React
-          </div>
-          <div class="col-4">
-            30/12/24
-          </div>
-          <div class="col-2">
-            <button type="button" class="btn btn-danger">
-              Delete
-            </button>
-          </div>
-        </div>
+      <TodoItem1 />
+      <TodoItem2 />
     </center>
   );
 }
